cb0b6d00e9c0885623841828352179baa7353165
Victor90001 <90458784+Victor90001@users.noreply.github.com>
2022-04-26 16:23:32 +0300

    Update index.php

--- 5/index.php
+++ 5/index.php
@@ -1,29 +1,14 @@
 <?php
 
-/**
- * Реализовать возможность входа с паролем и логином с использованием
- * сессии для изменения отправленных данных в предыдущей задаче,
- * пароль и логин генерируются автоматически при первоначальной отправке формы.
- */
 session_start();
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // Массив для временного хранения сообщений пользователю.
   $messages = array();
-  // В суперглобальном массиве $_COOKIE PHP хранит все имена и значения куки текущего запроса.
-  // Выдаем сообщение об успешном сохранении.
   if (!empty($_COOKIE['save'])) {
-    // Удаляем куку, указывая время устаревания в прошлом.
     setcookie('save', '', 100000);
     setcookie('login', '', 100000);
     setcookie('pass_in', '', 100000);
-    // Выводим сообщение пользователю.
     $messages[] = 'Спасибо, результаты сохранены.';
-    // Если в куках есть пароль, то выводим сообщение.
     if (!empty($_COOKIE['pass_in'])) {
       $messages[] = sprintf('Вы можете <a href="login.php">войти</a> с логином <strong>%s</strong>
         и паролем <strong>%s</strong> для изменения данных.',
@@ -42,7 +27,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     setcookie('privacy_value', '', 100000);
   }
 
-  // Складываем признак ошибок в массив.
   $errors_ar = array();
   $error=FALSE;
   $errors_ar['fio'] = !empty($_COOKIE['fio_error']);
@@ -87,8 +71,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $messages[] = '<div class="error">Необходимо согласиться с политикой конфиденциальности.</div>';
     $error=TRUE;
   }
-  // Складываем предыдущие значения полей в массив, если есть.
-  // При этом санитизуем все данные для безопасного отображения в браузере.
   $values = array();
   $values['fio'] = empty($_COOKIE['fio_value']) ? '' : strip_tags($_COOKIE['fio_value']);
   $values['mail'] = empty($_COOKIE['mail_value']) ? '' : strip_tags($_COOKIE['mail_value']);
@@ -100,15 +82,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $values['levitation'] = empty($_COOKIE['levitation_value']) ? 0 : $_COOKIE['levitation_value'];
   $values['bio'] = empty($_COOKIE['bio_value']) ? '' : strip_tags($_COOKIE['bio_value']);
   $values['privacy'] = empty($_COOKIE['privacy_value']) ? FALSE : $_COOKIE['privacy_value'];
-  // Если нет предыдущих ошибок ввода, есть кука сессии, начали сессию и
-  // ранее в сессию записан факт успешного логина.
-  //print_r(empty($_SESSION['login']).' '.$_COOKIE[session_name()].' '.empty($_SESSION['uid']));
   if (!$error and !empty($_COOKIE[session_name()]) and !empty($_SESSION['login'])) {
-    $user = 'u41026';
-    $pass = '4433573';
-    $db2 = new PDO('mysql:host=localhost;dbname=u41026', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    require('connect.php');
     try{
-      $get=$db2->prepare("select * from application where id=?");
+      $get=$db->prepare("select * from application where id=?");
       $get->bindParam(1,$_SESSION['uid']);
       $get->execute();
       $inf=$get->fetchALL();
@@ -119,7 +96,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       $values['limb']=$inf[0]['limb'];
       $values['bio']=$inf[0]['bio'];
 
-      $get2=$db2->prepare("select power from powers where id=?");
+      $get2=$db->prepare("select power from powers where id=?");
       $get2->bindParam(1,$_SESSION['uid']);
       $get2->execute();
       $inf2=$get2->fetchALL();
@@ -139,19 +116,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       print('Error: '.$e->getMessage());
       exit();
     }
-    // TODO: загрузить данные пользователя из БД
-    // и заполнить переменную $values,
-    // предварительно санитизовав.
     printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
   }
-  // Включаем содержимое файла form.php.
-  // В нем будут доступны переменные $messages, $errors и $values для вывода 
-  // сообщений, полей с ранее заполненными данными и признаками ошибок.
   include('form.php');
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 else {
-  // Проверяем ошибки.
   $fio=$_POST['fio'];
   $mail=$_POST['mail'];
   $year=$_POST['year'];
@@ -237,8 +206,10 @@ else {
       }
     }
   }
+  
   //запись куки для биографии
   setcookie('bio_value',$bio,time()+ 60*60);
+  
   //проверка согласия с политикой конфиденциальности
   if(empty($_SESSION['login'])){
     if(!isset($priv)){
@@ -266,13 +237,9 @@ else {
     setcookie('privacy_error', '', 100000);
   }
   
-  $user = 'u41026';
-  $pass = '4433573';
-  $db = new PDO('mysql:host=localhost;dbname=u41026', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-  // Проверяем меняются ли ранее сохраненные данные или отправляются новые.
+  require('connect.php');
   if (!empty($_COOKIE[session_name()]) && !empty($_SESSION['login']) and !$errors) {
     $id=$_SESSION['uid'];
-    
     $upd=$db->prepare("update application set name=:name,mail=:mail,date=:date,sex=:sex,limb=:limb,bio=:bio where id=:id");
     $cols=array(
       ':name'=>$fio,
@@ -335,11 +302,8 @@ else {
       }
     }
   }
-
   if(!$errors){
     setcookie('save', '1');
   }
-
-  // Делаем перенаправление.
   header('Location: ./');
 }
