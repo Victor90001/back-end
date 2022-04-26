b7da010e020543d1f29b88ed1f66c2962389bf0e
Victor90001 <u41026@webdev.fpm.kubsu.ru>
2022-04-26 08:54:59 +0300

    add auth

--- 6/edit.php
+++ 6/edit.php
@@ -5,6 +5,28 @@
 //echo '<pre>';
 //print_r($_GET);
 //echo '</pre>';
+require('connect.php');
+$pass_hash=array();
+try{
+  $get=$db->prepare("select password from administration where username=?");
+  $get->execute(array('admin'));
+  $pass_hash=$get->fetchAll()[0][0];
+}
+catch(PDOException $e){
+  print('Error: '.$e->getMessage());
+}
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+      empty($_SERVER['PHP_AUTH_PW']) ||
+      $_SERVER['PHP_AUTH_USER'] != 'admin' ||
+      md5($_SERVER['PHP_AUTH_PW']) != $pass_hash) {
+    header('HTTP/1.1 401 Unanthorized');
+    header('WWW-Authenticate: Basic realm="My site"');
+    print('<h1>401 Требуется авторизация</h1>');
+    exit();
+}
+if(empty($_GET['edit_id'])){
+  header('Location: admin.php');
+}
 header('Content-Type: text/html; charset=UTF-8');
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $messages = array();
